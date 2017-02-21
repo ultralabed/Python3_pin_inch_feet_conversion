c35d72d645d3db95fa822a539d1b2cc049a36b84
ed <ultralabed@gmail.com>
2017-02-21 12:29:45 +0800

    deploy(Converter): Build converter app with new feature (Warning messages/better layout/fixed app width)

--- dist/converter.app/Contents/Resources/converter.py
+++ dist/converter.app/Contents/Resources/converter.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import StringVar
+from tkinter import messagebox
 
 lan='chinese'
 
@@ -11,13 +12,15 @@ converBtn="轉換" if lan == 'chinese' else "convert"
 language="語系" if lan == 'chinese' else "Language"
 lanBtnEn="英文" if lan == 'chinese' else "English"
 lanBtnCh="中文" if lan == 'chinese' else "Chinese"
-test1="面積換算器" if lan == 'chinese' else "Transformation Area Converter"
+title="面積換算器" if lan == 'chinese' else "Transformation Area Converter"
+warningMsg="請填入至少一個數字" if lan == 'chinese' else "Please enter at least one number for convertion"
 
 window=tk.Tk()
-window.title(test1)
-window.minsize(width=425, height=135)
-window.maxsize(width=425, height=135)
-eleWidth=11
+window.title(title)
+window.geometry('430x135+30+0')
+window.resizable(False, False)
+eleWidth=14
+eleEntryWidth=16
 
 def changeLan():
     l1["text"]=calfootage2="坪" if lan == 'chinese' else "Square footage"
@@ -27,8 +30,10 @@ def changeLan():
     l0["text"]=language="語系" if lan == 'chinese' else "Language"
     btnEnglish["text"]=lanBtnEn="英文" if lan == 'chinese' else "English"
     btnChinese["text"]=lanBtnCh="中文" if lan == 'chinese' else "Chinese"
-    test1="面積換算器" if lan == 'chinese' else "Converter"
-    window.title(test1)
+    title="面積換算器" if lan == 'chinese' else "Converter"
+    global warningMsg
+    warningMsg="請填入至少一個數字" if lan == 'chinese' else "Please enter at least one number for convertion"
+    window.title(title)
 
 def chagneLanEn():
     global lan
@@ -40,68 +45,76 @@ def chagneLanCh():
     lan='chinese'
     changeLan()
 
-l0=tk.Label(window, text=language, width=eleWidth)
-l0.grid(row=0, column=0)
+l0=tk.Label(window, text=language, width=eleWidth, anchor='w')
+l0.grid(row=0, column=0, padx=10)
 btnEnglish=tk.Button(window, text=lanBtnEn, command=chagneLanEn, width=eleWidth)
 btnEnglish.grid(row=0, column=1)
 btnChinese=tk.Button(window, text=lanBtnCh, command=chagneLanCh, width=eleWidth)
 btnChinese.grid(row=0, column=2)
 
 def convert_square_footage():
+    if(e1_value.get() != ''):
+        feets=round(float(e1_value.get())*35.583185317, 3)
+        e3.delete(0, tk.END)
+        e3.insert(0, feets)
 
-    feets=round(float(e1_value.get())*35.587, 3)
-    e3.delete(0, tk.END)
-    e3.insert(0, feets)
-
-    m2=round(float(e1_value.get())*3.305, 3)
-    e2.delete(0, tk.END)
-    e2.insert(0, m2)
+        m2=round(float(e1_value.get())*3.3057862168, 3)
+        e2.delete(0, tk.END)
+        e2.insert(0, m2)
+    else:
+        messagebox.showinfo(message=warningMsg)
 
 def convert_square_meters():
-    feets=round(float(e2_value.get())*10.76, 3)
-    e3.delete(0, tk.END)
-    e3.insert(0, feets)
+    if(e2_value.get() != ''):
+        feets=round(float(e2_value.get())*10.76391, 3)
+        e3.delete(0, tk.END)
+        e3.insert(0, feets)
 
-    pin=round(float(e2_value.get())/3.305, 3)
-    e1.delete(0, tk.END)
-    e1.insert(0, pin)
+        pin=round(float(e2_value.get())*0.3024999, 3)
+        e1.delete(0, tk.END)
+        e1.insert(0, pin)
+    else:
+        messagebox.showinfo(message=warningMsg)
 
 def convert_square_feets():
-    m2=round(float(e3_value.get())*0.093, 3)
-    e2.delete(0, tk.END)
-    e2.insert(0, m2)
+    if(e3_value.get() != ''):
+        m2=round(float(e3_value.get())*0.0929030436, 3)
+        e2.delete(0, tk.END)
+        e2.insert(0, m2)
 
-    pin=round(float(e3_value.get())/35.587, 3)
-    e1.delete(0, tk.END)
-    e1.insert(0, pin)
+        pin=round(float(e3_value.get())*0.0281031614, 3)
+        e1.delete(0, tk.END)
+        e1.insert(0, pin)
+    else:
+        messagebox.showinfo(message=warningMsg)
 
 ttk.Separator(window,orient='horizontal').grid(row=1, columnspan=3, sticky="ew")
 
-l1=tk.Label(window, text=calfootage2, width=eleWidth)
+l1=tk.Label(window, text=calfootage2, width=eleWidth, anchor='w')
 l1.grid(row=2, column=0)
 
 e1_value=tk.StringVar()
-e1=tk.Entry(window, textvariable=e1_value)
+e1=tk.Entry(window, textvariable=e1_value, width=eleEntryWidth)
 e1.grid(row=2, column=1)
 
 b1=tk.Button(window, text=converBtn, command=convert_square_footage, width=eleWidth)
 b1.grid(row=2, column=2)
 
-l2=tk.Label(window, text=calmeter2, width=eleWidth)
+l2=tk.Label(window, text=calmeter2, width=eleWidth, anchor='w')
 l2.grid(row=3, column=0)
 
 e2_value=tk.StringVar()
-e2=tk.Entry(window, textvariable=e2_value)
+e2=tk.Entry(window, textvariable=e2_value, width=eleEntryWidth)
 e2.grid(row=3, column=1)
 
 b2=tk.Button(window, text=converBtn, command=convert_square_meters, width=eleWidth)
 b2.grid(row=3, column=2)
 
-l3=tk.Label(window, text=calfeet2, width=eleWidth)
+l3=tk.Label(window, text=calfeet2, width=eleWidth, anchor='w')
 l3.grid(row=4, column=0)
 
 e3_value=tk.StringVar()
-e3=tk.Entry(window, textvariable=e3_value)
+e3=tk.Entry(window, textvariable=e3_value, width=eleEntryWidth)
 e3.grid(row=4, column=1)
 
 b3=tk.Button(window, text=converBtn, command=convert_square_feets, width=eleWidth)
